b4f565d1b0c1fddec8c95cba0a9fc6ff433e7499
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2024-01-15 18:01:13 -0500

    intergrate clap with code

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,30 @@
 use rand::Rng;
+use clap::Parser;
 
 const LOWERCASE_CHARS: [&str; 26] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const UPPERCASE_CHARS: [&str; 26] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const NUMERIC_CHARS: [&str; 10] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 const SPECIAL_CHARS: [&str; 27] = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "_", "+", "[", "]", "{", "}", "|", ";", ":", ",", ".", "<", ">", "?", "/"];
 
+/// Simple password generator
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Length of the password
+    #[arg(short, long, default_value_t = 32)]
+    length: u8,
+}
+
 fn main() {
+    let args = Args::parse();
+
     let mut rng = rand::thread_rng();
 
     let mut password = String::new();
-    let pool = 0;
+    let pool = 4;
 
-    let mut i = 1;
-    while i < 32 {
+    let mut i = 0;
+    while i < args.length {
         if pool == 0 {
             println!("Please provide a char type");
             break;
