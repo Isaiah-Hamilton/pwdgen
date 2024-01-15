acf250ecfb4b6e289d2758b4acb56df7e9040277
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2024-01-15 17:03:16 -0500

    init password generator

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
-use clap::Parser;
+use rand::Rng;
 
-/// Simple program to greet a person
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    name: String,
-
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
-}
+const LOWERCASE_CHARS: [&str; 26] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const UPPERCASE_CHARS: [&str; 26] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const NUMERIC_CHARS: [&str; 10] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const SPECIAL_CHARS: [&str; 27] = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "_", "+", "[", "]", "{", "}", "|", ";", ":", ",", ".", "<", ">", "?", "/"];
 
 fn main() {
-    let args = Args::parse();
+    let mut rng = rand::thread_rng();
 
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name)
+    let mut password = String::new();
+
+    let mut i = 1;
+    while i < 32 {
+    let random_number = rng.gen_range(0..=LOWERCASE_CHARS.len());
+        password += LOWERCASE_CHARS[random_number];
+        i += 1;
     }
+
+    println!("password: {}", password);
 }
